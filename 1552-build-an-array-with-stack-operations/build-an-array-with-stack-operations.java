5e22b63ef48904b9f1ba86ccb6c08a46d7debf32
ESWARREDDY <eswarreddykoduru@gmail.com>
2026-09-19 14:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (70.16%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -1,22 +1,17 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
-        int arr []= new int[n];
         List<String> st = new Stack<>();
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=i+1;
-        }
-        int j=0;
+        int j=1;
         int k=0;
         while(k!=target.length)
         {
-            if(arr[j]==target[k])
+            if(j==target[k])
             {
               st.add("Push");
               k++;
               j++;
             }
-            else  if(arr[j]!=target[k])
+            else  if(j!=target[k])
             {
                 st.add("Push");
                 st.add("Pop");
